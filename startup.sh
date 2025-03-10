e981f1b156d548758039549ad6b83ccdf833a7eb
Pyroconiami <pyroconiomi@gmail.com>
2025-03-10 14:35:34 -0400

    Add debugging statements to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+echo "Starting startup.sh script..."
+
 # Remove the existing node_modules directory
+echo "Removing node_modules directory..."
 rm -rf node_modules
 
 # Reinstall dependencies
+echo "Reinstalling dependencies..."
 npm install
 
 # Rebuild better-sqlite3 from source
+echo "Rebuilding better-sqlite3 from source..."
 npm rebuild better-sqlite3 --build-from-source
 
 # Start the bot
+echo "Starting the bot..."
 node index.js
