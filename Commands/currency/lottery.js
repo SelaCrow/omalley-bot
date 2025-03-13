57a4c36e8899fd1a6cf12ccfd3a444eadd85e086
Pyroconiami <pyroconiomi@gmail.com>
2025-03-13 15:37:00 -0400

    lottery update

--- Commands/currency/lottery.js
+++ Commands/currency/lottery.js
@@ -3,6 +3,7 @@ const { User } = require('../../dbObjects.js');
 
 const lotteryPool = {};
 const ticketPrice = 10;
+let prizePool = 0;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -22,6 +23,11 @@ module.exports = {
 			subcommand
 				.setName('check')
 				.setDescription('Check how many lottery tickets you have'),
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('pool')
+				.setDescription('Check the total gold in the prize pool'),
 		),
 
 	async execute(interaction) {
@@ -31,6 +37,11 @@ module.exports = {
 		// 🛒 `/lottery buy` - Purchase Tickets
 		if (subcommand === 'buy') {
 			const ticketCount = interaction.options.getInteger('tickets');
+
+			if (ticketCount <= 0) {
+				return interaction.reply({ content: '❌ You must buy at least 1 ticket.', ephemeral: true });
+			}
+
 			const totalCost = ticketCount * ticketPrice;
 
 			// Retrieve user from the database
@@ -45,14 +56,20 @@ module.exports = {
 			await user.save();
 
 			lotteryPool[userId] = (lotteryPool[userId] || 0) + ticketCount;
+			prizePool += totalCost;
 
-			return interaction.reply(`🎟️ You bought **${ticketCount}** tickets! You now have **${lotteryPool[userId]}** total.`);
+			return interaction.reply(`🎟️ You bought **${ticketCount}** tickets! You now have **${lotteryPool[userId]}** total. The prize pool is now **${prizePool}** gold!`);
 		}
 
 		// 🔎 `/lottery check` - Check User's Tickets
 		if (subcommand === 'check') {
 			const userTickets = lotteryPool[userId] || 0;
 			return interaction.reply(`🎟️ You have **${userTickets}** lottery tickets.`);
+		}
+
+		// 💰 `/lottery pool` - Check Total Prize Pool (Gold)
+		if (subcommand === 'pool') {
+			return interaction.reply(`🏆 The total prize pool is **${prizePool}** gold!`);
 		}
 	},
 };
