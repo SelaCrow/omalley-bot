3319b712f0d42a4329ee64cdb1ab7945de61f11d
marisela <mariselaalejandragomez@gmail.com>
2026-04-16 08:11:15 -0700

    update bot to use environment variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const token = process.env.DISCORD_TOKEN;
 const { sequelize, User } = require('./dbObjects.js');
 
 const client = new Client({
