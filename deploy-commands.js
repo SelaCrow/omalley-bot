cc2e62dd3444f61735f081aa566e68201b0cdd91
marisela <mariselaalejandragomez@gmail.com>
2026-04-16 08:03:00 -0700

    fix command folder casing for Linux deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
-const token = process.env.DISCORD_TOKEN;
+const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
+const { token } = require('./config.json');
 const { sequelize, User } = require('./dbObjects.js');
 
 const client = new Client({
